a15d11ed41639aa42ddeec7595dcd75339ea860b
Akash <Akash.patel.ap88020@gmail.com>
2025-04-17 18:21:33 +0530

    leart about bcrypt or zod and apply in the todo project

--- Developemnt/mongoTodo/middleware.js
+++ Developemnt/mongoTodo/middleware.js
@@ -2,43 +2,78 @@ const express = require('express');
 const {UserModel,TodoModel} = require('./db');
 const jwt = require('jsonwebtoken');
 const jwt_secret = "key";
+const bcrypt = require('bcrypt');
+const {z} = require('zod');
 
 const signUp =  async (req,res)=> {
+    const requiredBody = z.object({
+        email : z.string().min(3).max(100).email(),
+        password : z.string().min(3).max(30),
+        name : z.string().min(3).max(100),
+    })
+    const parseDataWithSuccess = requiredBody.parse(req.body);
+    if(!parseDataWithSuccess){
+        res.json({
+            message : "Incorrect formate",
+        })
+        return;
+    }
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    await UserModel.create({
-        email : email,
-        password : password,
-        name : name,
-    })
+    try {
+        const hashPassword = await bcrypt.hash(password,5);
+        console.log(hashPassword);
     
-    res.json({
-        message : "You are logged In",
-    })
-    console.log(req.body);
+        await UserModel.create({
+            email : email,
+            password : hashPassword,
+            name : name,
+        })
+        
+        res.json({
+            message : "You are logged In",
+        })
+    } catch (error) {
+        res.json({
+            error : error,
+        })
+    }
 }
 const signIn = async (req,res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = await UserModel.findOne({
-        email : email,
-    })
-
-    if(!user){
-        res.status(403).json({
-            message : "incorrect credentials",
+    try {
+        const user = await UserModel.findOne({
+            email : email,
         })
-    }else{
-        const token = jwt.sign({id : user._id},jwt_secret);
-        res.status(200).json({
-            token : token,
-            user : {
-                name : user.name,
-                email : user.email,
-            }
+    
+        if(!user){
+            res.json({
+                message : "user is not found on DB", 
+            })
+        }
+        const passwordMatch = await bcrypt.compare(password,user.password);
+        
+        if(!passwordMatch){
+            res.status(403).json({
+                message : "password is wrong",
+            })
+        }else{
+            const token = jwt.sign({id : user._id},jwt_secret);
+            res.status(200).json({
+                token : token,
+                user : {
+                    name : user.name,
+                    email : user.email,
+                }
+            })
+        }
+    } catch (error) {
+        res.json({
+            error : error,
         })
     }
 }
@@ -47,25 +82,38 @@ const todo = async (req,res) => {
     const {title , done} = req.body;
     const userId = req.userId;
 
-    await TodoModel.create({
-        title,
-        done,
-        userId,
-    })
+    try {
+        await TodoModel.create({
+            title,
+            done,
+            userId,
+        })
+    
+        res.json({
+            userId : userId,
+            message : "Todos created"
+        })
+    } catch (error) {
+        res.json({
+            error : error
+        })
+    }
 
-    res.json({
-        userId : userId,
-        message : "Todos created"
-    })
 }
 
 const todos = async (req,res) => {
-    const userId = req.userId;
-    const todos =  await TodoModel.findOne({userId});
-    res.json({
-        todos : todos,
-    })
+    try {
+        const userId = req.userId;
+        const todos =  await TodoModel.findOne({userId});
+        res.json({
+            todos : todos,
+        })
+    } catch (error) {
+        res.json({
+            error : error,
+        })
+    }
 }
 
-module.exports = {signIn,signUp,todo,todos};
+module.exports = {signIn,signUp,todo,todos}; 
 
